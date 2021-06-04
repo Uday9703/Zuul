7c8c2b7f941788005890986be051776402100032
Uday9703 <62356590+Uday9703@users.noreply.github.com>
2021-06-04 14:08:31 +0530

    Add files via upload

---
+++ Gateway/src/main/java/Spring/Gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package Spring.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+@CrossOrigin("http://localhost:4200")
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
